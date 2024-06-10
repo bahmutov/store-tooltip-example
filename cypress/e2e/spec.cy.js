3d8202f0664d4a3e73fe4017286709135be775c0
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-06-09 22:00:51 -0400

    r1 lesson

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -8,28 +8,17 @@ it('adds an item to the cart via tooltip', { scrollBehavior: 'center' }, () => {
   // open the product page
   cy.visit('/index.php?route=product/manufacturer/info&manufacturer_id=8')
   // there should be several products on the page
-  cy.get('.product-thumb')
-    .should('have.length.greaterThan', 3)
-    // let the page load and take the first product
-    .wait(1000)
-    .first()
-    // within the ".product-thumb" element
-    .within(() => {
-      // the product action element should be invisible
-      // when we hover over the ".product-thumb-top" element
-      // the action element should become visible
-      // Tip: you need the "realHover" command from cypress-real-events
-      cy.get('.product-action').should('not.be.visible')
-      cy.get('.product-thumb-top').realHover()
-      cy.get('.product-action')
-        .should('be.visible')
-        // find the button "Add to Cart" and click it
-        .contains('button', 'Add to Cart')
-        .click()
-    })
+  // let the page load and take the first product
+  // within the ".product-thumb" element
+  //
+  // the product action element should be invisible
+  // when we hover over the ".product-thumb-top" element
+  // the action element should become visible
+  // Tip: you need the "realHover" command from cypress-real-events
+  //
+  // find the button "Add to Cart" and click it
 
   // the notification popup with text "View Cart" appears
-  cy.contains('#notification-box-top', 'View Cart').should('be.visible')
+  //
   // then the popup goes away after about 15 seconds
-  cy.get('#notification-box-top', { timeout: 15_000 }).should('not.be.visible')
 })
